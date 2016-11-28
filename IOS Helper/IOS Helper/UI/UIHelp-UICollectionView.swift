aca2a30144b1b50648468d29c52dad008ef0ffd8
Andrew Apperley <andrew@tinyhearts.com>
2016-11-27 21:39:51 -0500

    Added collection view component

--- IOS Helper/IOS Helper/UI/UIHelp-UICollectionView.swift
+++ IOS Helper/IOS Helper/UI/UIHelp-UICollectionView.swift
@@ -8,11 +8,39 @@
 
 import UIKit
 
-class UIHelp_UICollectionView: HelperViewController {
+class UIHelp_UICollectionView: HelperViewController, UICollectionViewDataSource {
+
+  let data = [UIColor.redColor(), UIColor.blackColor(), UIColor.greenColor(), UIColor.yellowColor(), UIColor.purpleColor(), UIColor.grayColor(), UIColor.cyanColor(), UIColor.brownColor(), UIColor.redColor(), UIColor.blackColor(), UIColor.greenColor(), UIColor.yellowColor(), UIColor.purpleColor(), UIColor.grayColor(), UIColor.cyanColor(), UIColor.brownColor(), UIColor.redColor(), UIColor.blackColor(), UIColor.greenColor(), UIColor.yellowColor(), UIColor.purpleColor(), UIColor.grayColor(), UIColor.cyanColor(), UIColor.brownColor(), UIColor.redColor(), UIColor.blackColor(), UIColor.greenColor(), UIColor.yellowColor(), UIColor.purpleColor(), UIColor.grayColor(), UIColor.cyanColor(), UIColor.brownColor()]
+  let kCellKey = "collectionViewCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      self.view.backgroundColor = UIColor.yellowColor()
+      self.title = "UICollectionView"
+
+      let layout = UICollectionViewFlowLayout()
+      layout.scrollDirection = .Horizontal
+      layout.itemSize = CGSize(width: 90, height: 90)
+      let collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
+      collectionView.backgroundColor = UIColor.clearColor()
+      collectionView.layer.borderColor = UIColor.blackColor().CGColor
+      collectionView.layer.borderWidth = 1
+      collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: kCellKey)
+      collectionView.dataSource = self
+      stack.addArrangedSubview(collectionView)
+
+      collectionView.heightAnchor.constraintEqualToConstant(300).active = true
+      collectionView.centerXAnchor.constraintEqualToAnchor(self.view.centerXAnchor).active = true
     }
+
+  //MARK: - UICollectionViewDataSource -
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return data.count
+  }
+
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier(kCellKey, forIndexPath: indexPath)
+    cell.contentView.backgroundColor = data[indexPath.row]
+    return cell
+  }
 
 }
--- IOS Helper/IOS Helper/UI/UIHelp-UISwitch.swift
+++ IOS Helper/IOS Helper/UI/UIHelp-UISwitch.swift
@@ -12,7 +12,16 @@ class UIHelp_UISwitch: HelperViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      self.view.backgroundColor = UIColor.yellowColor()
+      self.title = "UISwitch"
+
+      let uiSwitch = UISwitch()
+      uiSwitch.addTarget(self, action: #selector(switchValueChanged), forControlEvents: .ValueChanged)
+      stack.addArrangedSubview(uiSwitch)
     }
 
+  func switchValueChanged(sender: UISwitch) {
+    let alert = UIAlertController(title: "UISwitch", message: "UISwitch value changed is on: \(sender.on)", preferredStyle: .Alert)
+    alert.addAction(UIAlertAction(title: "Dismiss", style: .Default, handler: nil))
+    self.presentViewController(alert, animated: true, completion: nil)
+  }
 }
