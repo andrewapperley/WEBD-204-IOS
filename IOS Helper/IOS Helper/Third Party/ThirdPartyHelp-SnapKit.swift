c038b12070dadf217525de3a12f07531c485f76e
Andrew Apperley <andrew@tinyhearts.com>
2016-11-30 20:41:46 -0500

    Added cocoapods and filled in the third party SnapKit component

--- IOS Helper/IOS Helper/Third Party/ThirdPartyHelp-SnapKit.swift
+++ IOS Helper/IOS Helper/Third Party/ThirdPartyHelp-SnapKit.swift
@@ -7,12 +7,64 @@
 //
 
 import UIKit
+import SnapKit
 
-class ThirdPartyHelp_SnapKit: HelperViewController {
+class ThirdPartyHelp_SnapKit: HelperViewController, UICollectionViewDataSource {
+
+  let data = [UIColor.redColor(), UIColor.yellowColor(), UIColor.redColor(), UIColor.yellowColor(), UIColor.redColor(), UIColor.yellowColor(), UIColor.redColor(), UIColor.yellowColor(), UIColor.redColor(), UIColor.yellowColor(), UIColor.redColor(), UIColor.yellowColor()]
 
   override func viewDidLoad() {
     super.viewDidLoad()
     self.title = "SnapKit"
+
+    let button = UIButton()
+    button.backgroundColor = UIColor.greenColor()
+    button.setTitle("Tap me", forState: .Normal)
+
+    let _view = UIView()
+
+    stack.addArrangedSubview(_view)
+
+    let layout = UICollectionViewFlowLayout()
+    layout.itemSize = CGSize(width: 80, height: 80)
+
+    layout.scrollDirection = .Horizontal
+    let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+    collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+    collectionView.backgroundColor = UIColor.clearColor()
+    collectionView.dataSource = self
+
+    _view.addSubview(collectionView)
+    _view.addSubview(button)
+
+    _view.snp_makeConstraints { (make) in
+      make.height.equalTo(400)
+      make.width.equalToSuperview()
+    }
+
+    collectionView.snp_makeConstraints { (make) in
+      make.height.equalTo(300)
+      make.width.equalToSuperview()
+      make.top.equalTo(0)
+    }
+
+    button.snp_makeConstraints { (make) in
+      make.top.equalTo(collectionView.snp_bottom)
+      make.width.equalToSuperview()
+      make.bottom.equalTo(0)
+    }
+
   }
+
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return data.count
+  }
+
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath)
+    cell.contentView.backgroundColor = data[indexPath.row]
+    return cell
+  }
+
 
 }
---
+++ IOS Helper/Pods/Target Support Files/Pods-IOS Helper/Pods-IOS Helper-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_IOS_HelperVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_IOS_HelperVersionString[];
+
