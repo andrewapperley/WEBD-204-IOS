62fbf8d8994d99b2a0e1f85bd4b31e31fb6db955
Andrew Apperley <andrew@tinyhearts.com>
2016-12-04 19:49:20 -0500

    Added movie player component

--- IOS Helper/IOS Helper/Advanced/AdvancedHelp-MoviePlayer.swift
+++ IOS Helper/IOS Helper/Advanced/AdvancedHelp-MoviePlayer.swift
@@ -7,13 +7,37 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class AdvancedHelp_MoviePlayer: HelperViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  var player: AVPlayerViewController!
 
-        // Do any additional setup after loading the view.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    self.title = "Video Player"
+
+    let url = NSURL.fileURLWithPath(NSBundle.mainBundle().pathForResource("bunny", ofType: "mp4")!)
+    player = AVPlayerViewController()
+    let avPlayer = AVPlayer(URL: url)
+    player.player = avPlayer
+
+    let showButton = UIButton(type: .System)
+    showButton.addTarget(self, action: #selector(showMovie), forControlEvents: .TouchUpInside)
+    showButton.setTitle("Show Movie", forState: .Normal)
+    stack.addArrangedSubview(showButton)
+
+    showButton.translatesAutoresizingMaskIntoConstraints = false
+    showButton.heightAnchor.constraintEqualToConstant(44).active = true
+    showButton.centerXAnchor.constraintEqualToAnchor(stack.centerXAnchor).active = true
+
+  }
+
+  @objc private func showMovie() {
+    self.presentViewController(player, animated: true, completion: nil)
+  }
+
 
 }
